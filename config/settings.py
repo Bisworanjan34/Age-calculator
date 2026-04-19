6c778001231ec2995ed2cc15a8406681f47efbb6
bisworanjan_dev <bisworanjan34@gmail.com>
2026-04-19 18:01:25 +0530

    ready to deploy

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -85,7 +86,7 @@ DATABASES = {
     }
 }
 
-
+# ALLOWED_HOSTS = ["*"]  # Ya fir ['.vercel.app'] add karein
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
@@ -119,8 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+import os
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 REDIRECT_LOGIN_URL = "calculator:home"
 REDIRECT_LOGOUT_URL = "core:login"
 login_url = "core:login"
